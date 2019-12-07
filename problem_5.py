3650574ffd3a439b7774c380a19ecb6f92cdf8d3
Rita Rózsavölgyi <rvrita@me.com>
2019-12-07 15:33:11 -0800

    Create problem_5.py

---
+++ problem_5.py
@@ -0,0 +1,82 @@
+from collections import defaultdict
+
+## Represents a single node in the Trie
+class TrieNode:
+    def __init__(self):
+        ## Initialize this node in the Trie
+        self.is_word = False
+        self.children = defaultdict(TrieNode)
+        
+    def find_words(self, prefix):
+        matches = []
+        if self.is_word:
+            matches += [prefix]
+        for (char, node) in self.children.items():
+            matches += node.find_words(prefix + char)
+
+        return matches
+
+## The Trie itself containing the root node and insert/find functions
+class Trie:
+    def __init__(self):
+        ## Initialize this Trie (add a root node)
+        self.root = TrieNode()
+
+    def insert(self, word):
+        ## Add a word to the Trie
+        node = self.root
+        for char in word:
+            node = node.children[char]
+
+        node.is_word = True
+
+    def find(self, prefix):
+        ## Find the Trie node that represents this prefix
+        node = self.root
+        for char in prefix:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                return None
+        return node
+
+    def match(self, prefix):
+        ## Return all matching words in the tree
+        node = self.find(prefix)
+        if node:
+            return node.find_words(prefix)
+        else:
+            return []
+
+MyTrie = Trie()
+wordList = [
+    "ant", "anthology", "antagonist", "antonym", 
+    "fun", "function", "factory", 
+    "trie", "trigger", "trigonometry", "tripod"
+]
+for word in wordList:
+    MyTrie.insert(word)
+
+
+print(MyTrie.match(""))
+print(MyTrie.match("ant"))
+print(MyTrie.match("anth"))
+print(MyTrie.match("f"))
+print(MyTrie.match("fu"))
+print(MyTrie.match("func"))
+print(MyTrie.match("tri"))
+print(MyTrie.match("trig"))
+
+# from IPython.display import display
+# from ipywidgets import widgets
+# from ipywidgets import interact
+# def f(prefix):
+#     if prefix != '':
+#         prefixNode = MyTrie.find(prefix)
+#         if prefixNode:
+#             print('\n'.join(prefixNode.suffixes()))
+#         else:
+#             print(prefix + " not found")
+#     else:
+#         print('')
+# interact(f,prefix='');
